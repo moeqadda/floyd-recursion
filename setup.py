3b4c8165939c58f4bfbe5568b66c8cbffb344655
moeqadda <136113468+moeqadda@users.noreply.github.com>
2023-07-06 00:47:58 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='recursive_floyd',
+    version='0.1',
+    description='A recursive implementation of the Floyd-Warshall algorithm',
+    long_description='...',
+    author='Mohamed Qadda',
+    author_email='M.Qadda@liverpool.ac.uk',
+    url='https://github.com/moeqadda/floyd-recursion/tree/main',
+    package_dir={'': 'src'},
+    packages=[''],
+    python_requires='>=3.6',
+    license='Apache 2.0',
+)
