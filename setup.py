08c012690e651b05b417daed19fe5e628fc58759
moeqadda <136113468+moeqadda@users.noreply.github.com>
2023-07-06 00:48:52 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='recursive_floyd',
     version='0.1',
     description='A recursive implementation of the Floyd-Warshall algorithm',
-    long_description='...',
+    long_description='A recursive implementation of the Floyd-Warshall algorithm',
     author='Mohamed Qadda',
     author_email='M.Qadda@liverpool.ac.uk',
     url='https://github.com/moeqadda/floyd-recursion/tree/main',
